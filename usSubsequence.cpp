7c76d7e9e590ad9247760c066208f25917b47a1d
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-06-20 17:46:25 +0530

    Create usSubsequence.cpp

---
+++ usSubsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0,j = 0;
+        
+        while(i<s.size() && j<t.size())
+        {
+            if(s[i] == t[j])
+            {
+                i+=1;
+            }
+            j+=1;
+        }
+        
+        if(i==s.size())
+        {
+            return true;
+        }
+        
+        return false;
+    }
+};
