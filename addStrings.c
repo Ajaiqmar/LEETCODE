d8d04ed1312cbc45e6968911de393189f9fe23df
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-06-30 21:48:55 +0530

    Update addStrings.c

--- addStrings.c
+++ addStrings.c
@@ -1,48 +1,48 @@
 char * addStrings(char * num1, char * num2){
     
-    int l1 = strlen(num1)-1, l2 = strlen(num2)-1,r = 0,i = 0;
+    int num1Length = strlen(num1)-1, num2Length = strlen(num2)-1,carry = 0,index = 0;
     char* ans = (char*)malloc(sizeof(char)*10002);
     
     
-    while(l1>=0 && l2>=0)
+    while(num1Length>=0 && num2Length>=0)
     {
-        int x = num1[l1]-'0';
-        int y = num2[l2]-'0';
+        int x = num1[num1Length]-'0';
+        int y = num2[num2Length]-'0';
         
-        ans[i] = '0'+((x+y+r)%10);
-        r = (x+y+r)/10;
+        ans[i] = '0'+((x+y+carry)%10);
+        carry = (x+y+carry)/10;
         
-        l1 -= 1;
-        l2 -= 1;
-        i += 1;
+        num1Length -= 1;
+        num2Length -= 1;
+        index += 1;
     }
     
-    while(l1>=0)
+    while(num1Length>=0)
     {
-        int x = num1[l1]-'0';
+        int x = num1[num1Length]-'0';
         
-        ans[i] = '0'+((x+r)%10);
-        r = (x+r)/10;
+        ans[index] = '0'+((x+carry)%10);
+        carry = (x+carry)/10;
         
-        l1 -= 1;
-        i += 1;
+        num1Length -= 1;
+        index += 1;
     }
     
-    while(l2>=0)
+    while(num2Length>=0)
     {
-        int y = num2[l2]-'0';
+        int y = num2[num2Length]-'0';
         
-        ans[i] = '0'+((y+r)%10);
-        r = (y+r)/10;
+        ans[index] = '0'+((y+carry)%10);
+        carry = (y+carry)/10;
         
-        l2 -= 1;
-        i += 1;
+        num2Length -= 1;
+        index += 1;
     }
     
-    if(r!=0)
+    if(carry!=0)
     {
-        ans[i] = '0'+r;
-        i+=1;
+        ans[index] = '0'+carry;
+        index+=1;
     }
     
     for(int j=0,k=i-1;j<k;j++,k--)
@@ -52,8 +52,8 @@ char * addStrings(char * num1, char * num2){
         ans[k] = temp;
     }
     
-    ans[i] = '\0';
-    i += 1;
+    ans[index] = '\0';
+    index += 1;
     
     return ans;
 }
