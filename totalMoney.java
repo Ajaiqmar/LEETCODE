387aca7ea4375c4add07a43b093644f8972cf44f
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-07-03 22:04:11 +0530

    Create totalMoney.java

---
+++ totalMoney.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int totalMoney(int n) {
+        int week = n/7;
+        int ans = 0;
+        
+        for(int i=0;i<week;i++)
+        {
+            ans += ((i+4)*7);
+        }
+        
+        int days = n%7;
+        
+        while(days>0)
+        {
+            ans += (week+1);
+            week+=1;
+            days-=1;
+        }
+        
+        return ans;
+    }
+}
