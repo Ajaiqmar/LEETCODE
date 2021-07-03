a22969882f2db5243552dc07a0d0b4857d239d98
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-07-03 22:51:02 +0530

    Update totalMoney.java

--- totalMoney.java
+++ totalMoney.java
@@ -1,3 +1,30 @@
+// NOT SO BETTER APPROACH
+
+class Solution {
+    public int totalMoney(int n) {
+        int week = n/7;
+        int ans = 0;
+        
+        for(int i=0;i<week;i++)
+        {
+            ans += ((i+4)*7);
+        }
+        
+        int days = n%7;
+        
+        while(days>0)
+        {
+            ans += (week+1);
+            week+=1;
+            days-=1;
+        }
+        
+        return ans;
+    }
+}
+
+// BETTER APPROACH
+
 class Solution {
     public int totalMoney(int n) {
         int week = n/7;
