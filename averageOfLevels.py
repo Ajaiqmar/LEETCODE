a60de49a72d942d226d7528b71e8f8530f34f158
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-07-06 12:31:33 +0530

    Update averageOfLevels.py

--- averageOfLevels.py
+++ averageOfLevels.py
@@ -2,7 +2,7 @@ class Solution:
     countOfNodes = []
     sumOfNodes = []
     
-    def inorder(self,root,level):
+    def preorder(self,root,level):
         if(root!=None):
             if(len(Solution.sumOfNodes) == level):
                 Solution.sumOfNodes.append(root.val)
@@ -10,14 +10,14 @@ class Solution:
             else:
                 Solution.sumOfNodes[level] += root.val
                 Solution.countOfNodes[level] += 1
-            self.inorder(root.left,level+1)
-            self.inorder(root.right,level+1)
+            self.preorder(root.left,level+1)
+            self.preorder(root.right,level+1)
     
     def averageOfLevels(self, root: TreeNode) -> List[float]:
         Solution.countOfNodes.clear()
         Solution.sumOfNodes.clear()
         
-        self.inorder(root,0);
+        self.preorder(root,0);
         
         for i in range(len(Solution.sumOfNodes)):
             Solution.sumOfNodes[i] /= Solution.countOfNodes[i]
