b676ee21f62d94e3457c92f765af055c75355e00
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-07-29 20:47:44 +0530

    Update MyHashMap.java

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,12 +1,14 @@
+// THE PROBLEM STATEMNET IS TO IMPLEMENT HASHMAP WITHOUT USING THE BUILT-IN HASHMAP FUNCTION.
+// SINCE THE KEY AND VALUE ARE IN INTEGER FORMAT, MY APPROACH IS TO INITIALISE AN ARRAY TO STORE THE VALUES IN THE ARRAY USING THE KEY.
+
 class MyHashMap {
     int HashMap[];
     
-    /** Initialize your data structure here. */
     public MyHashMap() {
         HashMap = new int[1000001];
     }
     
-    /** value will always be non-negative. */
+
     public void put(int key, int value) {
         if(value == 0)
         {
@@ -18,7 +20,6 @@ class MyHashMap {
         }
     }
     
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
         if(HashMap[key] == 0)
         {
@@ -32,8 +33,10 @@ class MyHashMap {
         return HashMap[key];
     }
     
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
         HashMap[key] = 0;
     }
 }
+
+// TIME COMPLEXITY : O(N)
+// SPACE COMPLEXITY : O(N)
