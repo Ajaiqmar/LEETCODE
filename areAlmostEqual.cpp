205ec6f8cd035a2d74c87bcac3ae732af00b6552
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-07-29 22:12:44 +0530

    Update areAlmostEqual.cpp

--- areAlmostEqual.cpp
+++ areAlmostEqual.cpp
@@ -1,14 +1,21 @@
+// THE PROBLEM STATEMENT IS TO CHECK WHETHER THE BOTH THE STRINGS ARE EQUAL BY MAKING ATMOST ONE SWAP OPERATION.
+
+// MY APPROACH IS TO CHCEK WHETHER BOTH THE STRING CONTAINS EQUAL NUMBER OF CHARACTERS, IF NOT RETURN FALSE, IF IT CONTAINS EQUAL NUMBER OF CHARACTERS THEN CHECK FOR NUMBER
+// OF MISPLACED CHARCTERS, IF IT IS EQUAL TO TWO OR ZERO RETURN TRUE, ELSE FALSE.
+
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
         int countOfCharacters1[26]={0},countOfCharacters2[26]={0};
         
+        // COUNTING THE NUMBER OF CHARCTERS.
         for(int i=0;i<s1.size();i++)
         {
             countOfCharacters1[s1[i]-'a']++;
             countOfCharacters2[s2[i]-'a']++;
         }
         
+        // CHECKING IF THE OCCURRENCES MATCH.
         for(int i=0;i<26;i++)
         {
             if(countOfCharacters1[i]!=countOfCharacters2[i])
@@ -19,6 +26,7 @@ public:
         
         int countOfMisplacedCharacters = 0;
         
+        // FINDING THE NUMBER OF MISPLACED CHARCTERS.
         for(int i=0;i<s1.size();i++)
         {
             if(s1[i]!=s2[i])
@@ -27,6 +35,7 @@ public:
             }
         }
         
+        // CHECKING WHETHER THE COUNT OF MISPLACED CHARACTER IS EQUAL TO 0 OR 2.
         if(countOfMisplacedCharacters == 0 || countOfMisplacedCharacters == 2)
         {
             return true;
