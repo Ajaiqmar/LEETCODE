57d652082eb67cfc515ad1201bc2bfc093a6de4e
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-08-05 12:24:29 +0530

    Update buddyStrings.py

--- buddyStrings.py
+++ buddyStrings.py
@@ -1,11 +1,27 @@
+# THE PROBLEM STATEMENT IS TO CHECK WHETHER THE GIVEN STRINGS IS BUDDY STRING. A STRING IS CALLED A BUDDY STRING IF BOTH THE STRINGS
+# BECOMES EQUAL AFTER EXACTLY SWAPPING TWO CHARACTERS IN ANY ONE OF THE STRING.
+
+# MY APPROACH IS TO DIVIDE THE PROBLEM INTO SUB PROBLEMS AND THEN WORKING THEM OUT INDIVIDUALLY.
+# 1. CHECK WHETHER THEY ARE OF EQUAL LENGTH. IF NOT RETURN FALSE
+
+# 2. NEXT CHECK WHETHER BOTH THE STRINGS CONTAIN EQUAL NUMBER OF ELEMENTS.
+
+# 3. NEXT, CHECK IF THEY ARE BOTH ALREADY EQUAL AND IF THEY ARE, CHECK WHETHER THERE ARE REPEATED CHARACTERS IN THE STRING, IF BOTH THE CONDITION
+#    IS TRUE RETURN TRUE.
+
+# 4. NEXT CHECK IF THERE ARE EXACTLY TWO MKISPLACED CHRACTERS, IF IT IS RETURN TRUE ELSE FALSE.
+
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
+        # CHECKING IF THEY ARE EQUAL IN LENGTH
         if(len(s) != len(goal) or len(s) == 1):
             return False
         
         alphad = {}
+        # FLAG VARIABLE IS TO CHECK WHETHER THE GIVEN STRINGS CONTAINS REPEATED CHARACTERS.
         flag = 0
         
+        # CHECKING BOTH THE STRINGS CONTAIN EQUAL NUMBER OF ELEMENTS USING A DICTIONARY
         for i in s:
             if i in alphad:
                 alphad[i] += 1
@@ -13,6 +29,7 @@ class Solution:
             else:
                 alphad[i] = 1
         
+        # IF BOTH THE STRINGS ARE EQUAL AND THEY CONTAIN REPEATED CHARACTERS.
         if(s == goal and flag == 1):
             return True
         
@@ -24,8 +41,12 @@ class Solution:
         
         countOfMisplaced = 0
         
+        # CHECKING FOR THE NUMBER OF MISPLACED CHARCTERS.
         for i in range(len(s)):
             if(s[i] != goal[i]):
                 countOfMisplaced += 1
         
         return countOfMisplaced == 2
+
+# TIME COMPLEXITY : O(MAX(N,M)) WHERE N IS THE LENGTH OF STRING 1 AND M IS THE LENGTH OF STRING2.
+# SPACE COMPLEXITY : O(N)
