a53cdf7e683441a52672ae0f65c50387d3eec18b
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-08-05 14:49:20 +0530

    Update canPlaceFlowers.c

--- canPlaceFlowers.c
+++ canPlaceFlowers.c
@@ -1,4 +1,9 @@
+// THE PROBLEM STATEMENT IS TO FIND OUT WHETHER WE COULD PLANT ATLEAST N FLOWERS IN A FLOWER BED. WHILE PLACING A FLOWER THE ADJACENT PLOTS MUST BE EMPTY.
+// 1 REPRESENT FLOWER AND 0 REPRESENTS EMPTY PLOT IN THE FLOWER BED.
 
+// MY APPROACH IS TO TRAVERSE TH FLOWERBED ARRAY AND CHECK AT EACH INDEX WHETHER IT IS A EMPTY PLOT, IF IT IS THEN CHECK WHTHER THE ADJACENT PLOTRS ARE EMPTY TOO.
+// IF IT IS THEN PLACE A FLOWER AND INCREMENT THE COUNTOFFREEBEDS VARIABLE. FINALLY CHECK WHETHER THE COUNTOFFREEBEDS IS GREATER THAN OR EQUAL TO N. IF IT IS RETURN TRUE,
+// ELSE RETURN FALSE.
 
 bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
     int countOfFreeBeds = 0;
@@ -14,6 +19,7 @@ bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
         return countOfFreeBeds >= n;
     }
     
+    // TARVERSING THE FLOWERBED ARRAY TO CHECK FOR EMPTY PLOTS.
     for(int i=0;i<flowerbedSize;i++)
     {
         if(i == 0 && flowerbed[i] == 0 && flowerbed[i+1]==0)
@@ -35,3 +41,6 @@ bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n){
     
     return countOfFreeBeds >= n;
 }
+
+// TIME COMPLEXITY : O(N)
+// SPACE COMPLEXITY : O(1)
