fafcbed46e7d61865daeace2befe0cf4f5af61f3
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-08-17 16:42:10 +0530

    Update checkIfArrayIsSortedAndRotated.py

--- checkIfArrayIsSortedAndRotated.py
+++ checkIfArrayIsSortedAndRotated.py
@@ -1,7 +1,14 @@
+# THE PROBLEM STATEMENT IS TO CHECK IF THE ARRAY IS SORTED IN ASCENDING ORDER AND ROTATED.
+
+# MY APPROACH IS TO TRAVERSE THE ARRAY AND COUNT THE NUMBER OF DIP COUNTS. A DIP IS WHEN THE PREVIOUS ELEMENT IS GREATER THAN THE CURRENT ELEMENT. IT IS ALSO NECESSARY TO COUNT 
+# THE FIRST AND LAST ELEMENT. IF THE DIP COUNT IS LESS THAN OR EQUAL TO 1 (PS: THE DIP COUNT CAN ALSO BE ZERO IF ALL THE
+# ELEMENTS ARE SAME.) RETURN TRUE, ELSE FALSE.
+
 class Solution:
     def check(self, nums: List[int]) -> bool:
         dipCount = 0
         
+        # TRAVBERSE THE ARRAY NUMS TO CHECK FOR DIPS.
         for i in range(1,len(nums)):
             if(nums[i]<nums[i-1]):
                 dipCount += 1
@@ -13,3 +20,6 @@ class Solution:
             return True
         
         return False
+
+# TIME COMPLEXITY : O(N)
+# SPACE COMPLEXITY : O(1)
