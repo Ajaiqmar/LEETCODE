e0158d6703fb83afcc4e51901bfcf23d569b89db
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-08-18 20:28:09 +0530

    Update checkOnesSegment.cpp

--- checkOnesSegment.cpp
+++ checkOnesSegment.cpp
@@ -1,8 +1,14 @@
+// THE PROBLEM STATEMENT IS TO CHECK WHETHER THE GIVEN STRING CONTAINS ATMOST ONE CONTIGOUS SEGMENT OF ONES. THE GIVEN STRING WILL ONLY CONTAIN '1' OR '0'.
+
+// MY APPROACH IS TO FIND THE LENGTH OF LONGEST CONTIGOUS SEGMENT OF ONES USING THE MODIFIED KADANE ALGORITHM. THEN CHECK IF THE COUNT OF ONES IS EQUAL 
+// TO THE LENGTH OF LONGEST CONTIGOUS SEGMENT OF ONES.
+
 class Solution {
 public:
     bool checkOnesSegment(string s) {
         int longestContigousSegment = 0,countOfOnes = 0,countOfSegments = 0;
         
+        // TRAVERSING THE ARRAY TO FIND THE LENGTH OF THE LONGEST CONTIGOUS SEGMENT OF ONES.
         for(int i=0;i<s.size();i++)
         {
             if(s[i] == '1')
@@ -24,3 +30,6 @@ public:
         return longestContigousSegment==countOfOnes;
     }
 };
+
+// TIME COMPLEXITY : O(N)
+// SPACE COMPLEXITY : O(1)
