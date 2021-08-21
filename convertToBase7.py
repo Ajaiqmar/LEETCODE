b7eb0e63154dcefce06c8913296fbacdeabc1be4
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-08-21 19:58:18 +0530

    Update convertToBase7.py

--- convertToBase7.py
+++ convertToBase7.py
@@ -1,8 +1,13 @@
+# THE PROBLEM STATEMENT IS TO CONVERT AND RETURN INTEGERS FROM BASE 10 TO BASE 7.
+
+# MY APPROACH IS TO DIVIDE THE INTEGER UNTIL IT IS GREATER THAN ZERO, AND AT EACH TRAVERSAL FIND THE REMAINDER AND ADD THAT VALUE TO THE ANS.
+
 class Solution:
     def convertToBase7(self, num: int) -> str:
         val = abs(num)
         ans = ""
         
+        # CONVERTING THE INTEGER FROM BASE 10 TO BASE 7
         while(val!=0):
             ans = str(val%7) + ans
             val = val//7
@@ -13,3 +18,6 @@ class Solution:
             ans = "0"
         
         return ans
+    
+# TIME COMPLEXITY : O(LOG N)
+#SPACE COMPLEXITY : O(1)
