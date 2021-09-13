fb177de2fdff99f6d0438a36d0cb4a646f2acf99
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-09-13 21:15:34 +0530

    Update wordPattern.py

--- wordPattern.py
+++ wordPattern.py
@@ -1,3 +1,9 @@
+# THE PROBLEM STATEMENT IS TO RETURN WHETHER THE CHARACTER PATTERN STRING MATCHES THE WORDS PATTERN IN THE SENTENCE S.
+
+# MY APPROACH IS TO FIRST CHECK DIVIDE THE SENTENCE INTO A LIST OF WORDS, THEN CHECK IF THE NUMBER OF CHARCTERS IN PATTERN IS SAME AS THE NUMBER OF WORDS IN
+# THE SENTENCE. IF IT IS CONTINUE, ELSE RETURN FALSE. NOW, WE HAVE TO STORE THE CHARACTER AND THE MATCHING WORD IN AN HASHMAP OR DICTIONARY.
+# IF THERE EXISTS A CHARACTER WHICH MAPE TO TWO DIFFERENT WORDS OR VICE VERSA THEN RETURN FALSE, ELSE RETURN TRUE.
+
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
@@ -8,6 +14,7 @@ class Solution:
         dp = {}
         ds = {}
         
+        # TRAVERSING THE PATTERN STRING AND THE LIST OF STRINGS.
         for i in range(len(s)):
             if pattern[i] in dp and s[i] in ds and dp[pattern[i]]!=ds[s[i]]:
                 return False
@@ -20,3 +27,6 @@ class Solution:
                 ds[s[i]] = i
         
         return True
+
+# TIME COMPLEXITY : O(N) WHERE N IS THE NUMBER OF CHARACTERS IN THE PATTERN STRING.
+# SPACE COMPLEXITY : O(N)
