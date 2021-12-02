89dd56ec487929671798888bcca1d7f4484284fa
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2021-12-02 18:53:39 +0530

    Update validPalindrome.c

--- validPalindrome.c
+++ validPalindrome.c
@@ -1,8 +1,12 @@
+// THE PROBLEM STATEMENT IS TO CHECK WHETHER THE GIVEN STRING IS A PALINDROME AFTER REMOVING ATMOST ONE CHARACTER FROM THE STRING.
+
+// MY APPROACH IS USE TWO POINTER APPROACH AND CHECK IF THE STRING IS A PALINDROME, WHILE TRAVERSING IF THE TWO POINTER VALUES DON'T MATCH
+// THEN CHECK WHETHER THE REST OF THE STRING IS PALINDROME BY SKIPPING EITHER THE LEFT POINTER OR THE RIGHT POINTER, IF IT IS RETURN TRUE ELSE FALSE.
 
 bool validPalindrome(char * s)
 {
     int left = 0,right = strlen(s)-1;
-    
+    // CHECKING WHETHER THE GIVEN STRING IS A PALINDROME. 
     while(left<right)
     {
         if(s[left] != s[right])
@@ -10,6 +14,7 @@ bool validPalindrome(char * s)
             bool ans = false;
             int tleft = left+1,tright = right,flag = 0;
             
+            // CHECKING WHETHER THE GIVEN STRING IS A PALINDROME BY SKIPPING THE LEFT POINTER.
             while(tleft<tright)
             {
                 if(s[tleft] != s[tright])
@@ -30,6 +35,7 @@ bool validPalindrome(char * s)
             tright = right-1;
             flag = 0;
             
+            // CHECKING WHETHER THE GIVEN STRING IS A PALINDROME BY SKIPPING THE RIGHT POINTER.
             while(tleft<tright)
             {
                 if(s[tleft] != s[tright])
@@ -54,3 +60,6 @@ bool validPalindrome(char * s)
     
     return true;
 }
+
+// TIME COMPLEXITY : O(N) WHERE N IS THE LENGTH OF THE GIVEN STRING.
+// SPACE COMPLEXITY : O(1)
